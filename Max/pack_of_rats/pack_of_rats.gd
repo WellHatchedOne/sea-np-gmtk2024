bbb06820fe1b250821c5f2208f789292b8649f2c
maxhuffy <42044782+maxhuffy@users.noreply.github.com>
2024-08-17 14:09:10 -0700

    added rats to be in a swarm parent, butchered ians original rat scene

---
+++ Max/pack_of_rats/pack_of_rats.gd
@@ -0,0 +1,60 @@
+extends CharacterBody2D
+class_name PackOfRats
+
+@export var speed: float = 400
+const RAT = preload("res://rat.tscn")
+@onready var pack_collision_circle = $CollisionShape2D
+
+var current_radius = 0
+var rat_children: Array = []
+
+func _input(event):
+	if event.is_action_pressed("change_color"):
+		spawn_rat(randf_range(-100, 100), randf_range(-100, 100))
+
+func get_input() -> void:
+	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	self.velocity = input_direction * speed;
+
+func _physics_process(delta) -> void:
+	get_input()
+	move_and_slide()
+	play_movement_animations()
+	
+func play_movement_animations():
+	for rat in rat_children:
+		if velocity.length() > 0:
+			rat.animated_sprite_2d.play()
+		else:
+			rat.animated_sprite_2d.stop()
+
+		if velocity.x != 0:
+			rat.animated_sprite_2d.flip_v = false
+			rat.animated_sprite_2d.flip_h = velocity.x < 0
+		elif velocity.y != 0:
+			if velocity.y > 0:
+				rat.animated_sprite_2d.animation = "down"
+			else:
+				rat.animated_sprite_2d.animation = "up"
+
+func _ready():
+	spawn_rat(0,0)
+
+func spawn_rat(x: float, y: float):
+	var new_rat = RAT.instantiate()
+	var new_position = Vector2(x, y)
+	var radius_from_parent_origin = Vector2(0,0).distance_to(new_position)
+	new_rat.position = new_position
+	
+	self.add_child(new_rat)
+	rat_children.append(new_rat)
+	
+	if (x==0 && y==0):
+		var rat_collision_shape = new_rat.find_child("CollisionShape2D") as CollisionShape2D
+		update_collision_radius(rat_collision_shape.shape.radius)
+	elif radius_from_parent_origin > current_radius:
+		current_radius = radius_from_parent_origin
+		update_collision_radius(radius_from_parent_origin)
+
+func update_collision_radius(new_radius):
+	pack_collision_circle.shape.radius = new_radius
--- ui/huds/minimap/rat_mini_map.gd
+++ ui/huds/minimap/rat_mini_map.gd
@@ -1,13 +1,14 @@
 extends CanvasLayer
 
-@export var test_movable_guy: CharacterBody2D = null
+@export var player: PackOfRats = null
 @onready var mm_camera = $Control/ColorRect/MarginContainer/SubViewportContainer/SubViewport/MMCamera
 @onready var sub_viewport = $Control/ColorRect/MarginContainer/SubViewportContainer/SubViewport
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	assert(player != null, "Player instance not assigned for Minimap")
 	sub_viewport.world_2d = get_viewport().world_2d
 
 func _physics_process(delta):
-	mm_camera.position = test_movable_guy.position
+	mm_camera.position = player.position
 	mm_camera.make_current()
