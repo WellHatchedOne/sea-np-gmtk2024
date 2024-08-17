66c0932fd03ea1ab477ac3a2057081649c6cc05e
Shawn <shawn.hatchwell@gmail.com>
2024-08-17 16:06:39 -0700

    new tileset

--- mapGen/dungeon.gd
+++ mapGen/dungeon.gd
@@ -9,11 +9,23 @@ extends Node2D
 
 var DEBUG = false
 
-var tile_size: int =  16
+var tile_size := 64
 
-var tileMapFloorVector: Vector2i = Vector2i(0, 8)
-var tileMapWallVector: Vector2i = Vector2i(0, 0)
-var tileMapCornerVector: Vector2i = Vector2i(5, 0)
+var source_id := 0
+
+var tileMapFloorVector := Vector2i(0, 8)
+var tileMapWallVector := Vector2i(0, 0)
+var tileMapCornerVector := Vector2i(5, 0)
+
+var tileMapTopAlt := 0
+var tileMapBottomAlt := 1
+var tileMapLeftAlt := 2
+var tileMapRightAlt := 3
+
+var tileMapTRAlt := 0
+var tileMapTLAlt := 5
+var tileMapBLAlt := 6
+var tileMapBRAlt := 7
 
 # END CONSTANTS
 
@@ -58,31 +70,31 @@ func _draw():
 				if not leaf.isNotFloorTile(x, y, padding):
 					# 1 is the atlas ID
 					# 0,8 is the tile location in the atlas
-					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), 1, tileMapFloorVector)
+					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), source_id, tileMapFloorVector)
 				elif not leaf.isNotFloorTile(x, y + 1, padding):
 					# Render top wall
-					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), 1, tileMapWallVector)
+					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), source_id, tileMapWallVector, tileMapTopAlt)
 				elif not leaf.isNotFloorTile(x, y - 1, padding):
 					# Render bottom wall
-					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), 1, tileMapWallVector, 5)
+					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), source_id, tileMapWallVector, tileMapBottomAlt)
 				elif not leaf.isNotFloorTile(x + 1, y, padding):
 					# Render left wall
-					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), 1, tileMapWallVector, 6)
+					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), source_id, tileMapWallVector, tileMapLeftAlt)
 				elif leaf.isNotFloorTile(x, y, padding) and not leaf.isNotFloorTile(x - 1, y, padding):
 					# Render right wall
-					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), 1, tileMapWallVector, 7)
+					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), source_id, tileMapWallVector, tileMapRightAlt)
 				elif leaf.isNotFloorTile(x, y, padding) and leaf.isNotFloorTile(x + 1, y, padding) and leaf.isNotFloorTile(x - 1, y, padding) and leaf.isNotFloorTile(x, y + 1, padding) and leaf.isNotFloorTile(x, y - 1, padding) and not leaf.isNotFloorTile(x + 1, y + 1, padding):
 					# Render top left wall
-					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), 1, tileMapCornerVector, 1)
+					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), source_id, tileMapCornerVector, tileMapTLAlt)
 				elif leaf.isNotFloorTile(x, y, padding) and leaf.isNotFloorTile(x + 1, y, padding) and leaf.isNotFloorTile(x - 1, y, padding) and leaf.isNotFloorTile(x, y + 1, padding) and leaf.isNotFloorTile(x, y - 1, padding) and not leaf.isNotFloorTile(x - 1, y + 1, padding):
 					# Render top right wall
-					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), 1, tileMapCornerVector)
+					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), source_id, tileMapCornerVector, tileMapTRAlt)
 				elif leaf.isNotFloorTile(x, y, padding) and leaf.isNotFloorTile(x + 1, y, padding) and leaf.isNotFloorTile(x - 1, y, padding) and leaf.isNotFloorTile(x, y + 1, padding) and leaf.isNotFloorTile(x, y - 1, padding) and not leaf.isNotFloorTile(x + 1, y - 1, padding):
 					# Render bottom left wall
-					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), 1, tileMapCornerVector, 3)
+					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), source_id, tileMapCornerVector, tileMapBLAlt)
 				elif leaf.isNotFloorTile(x, y, padding) and leaf.isNotFloorTile(x + 1, y, padding) and leaf.isNotFloorTile(x - 1, y, padding) and leaf.isNotFloorTile(x, y + 1, padding) and leaf.isNotFloorTile(x, y - 1, padding) and not leaf.isNotFloorTile(x - 1, y - 1, padding):
 					# Render bottom right wall
-					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), 1, tileMapCornerVector, 2)
+					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), source_id, tileMapCornerVector, tileMapBRAlt)
 					
 					
 	# Render room connections
@@ -90,11 +102,11 @@ func _draw():
 		if path['left'].y == path['right'].y:
 			# horizontal
 			for i in range(path['right'].x - path['left'].x):
-				tilemap.set_cell(0, Vector2i(path['left'].x+i,path['left'].y), 1, Vector2i(0, 9))
-				tilemap.set_cell(1, Vector2i(path['left'].x+i,path['left'].y), 1, Vector2i(0, 15))
+				tilemap.set_cell(0, Vector2i(path['left'].x+i,path['left'].y), source_id, Vector2i(0, 9))
+				tilemap.set_cell(1, Vector2i(path['left'].x+i,path['left'].y), source_id, Vector2i(0, 15))
 		else:
 			# vertical
 			for i in range(path['right'].y - path['left'].y):
-				tilemap.set_cell(0, Vector2i(path['left'].x,path['left'].y+i), 1, Vector2i(0, 11))
-				tilemap.set_cell(1, Vector2i(path['left'].x,path['left'].y+i), 1, Vector2i(0, 12))
+				tilemap.set_cell(0, Vector2i(path['left'].x,path['left'].y+i), source_id, Vector2i(0, 11))
+				tilemap.set_cell(1, Vector2i(path['left'].x,path['left'].y+i), source_id, Vector2i(0, 12))
 
