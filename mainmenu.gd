8fca9fe39a95285a408c1484c94e572c635455ef
Shawn <shawn.hatchwell@gmail.com>
2024-08-16 20:51:41 -0700

    edit correct file

--- mainmenu.gd
+++ mainmenu.gd
@@ -1,13 +1,13 @@
 extends Control
 
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+	
+func _on_exit_pressed():
+	get_tree().quit()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
