daa174a4c6fb158edbfd34475b9f98016cb96888
Shawn <shawn.hatchwell@gmail.com>
2024-08-16 21:25:47 -0700

    mainMenu hotfix

--- mainMenu/mainmenu.gd
+++ mainMenu/mainmenu.gd
@@ -4,10 +4,7 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://optionsMenu/optionsMenu.tscn")
 	
 func _on_exit_pressed():
 	get_tree().quit()
-
-
-
