98b03b377e9a181354d2c6dfcff58430bdb4be26
hackda-code <hackdah101@gmail.com>
2024-08-18 00:59:27 -0700

    dodge and music fix

--- ui/huds/hud/hud.gd
+++ ui/huds/hud/hud.gd
@@ -5,10 +5,15 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func onclick():
+		print("press")
+
+
 
