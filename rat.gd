a21e82f32038cd0442ac60c1f19faeb99978aa0a
Ian Brobin <ianbrobin@yahoo.com>
2024-08-18 15:16:23 -0700

    Add base logic for delay. Currently doesn't work, but delay is zero

--- rat.gd
+++ rat.gd
@@ -3,7 +3,11 @@ class_name Rat
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var collision_shape_2d = $CollisionShape2D
-var delay = 0
+var delay:float = 0
+var currentDeltaOffset:float = 0
+var currentPositionOffset:Vector2 = Vector2.ZERO
+var deltaQueue:Array[float] = []
+var positionQueue:Array[Vector2] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,13 +23,37 @@ func enable_collision():
 func set_delay(newDelay:float):
 	delay = newDelay
 
-func ratMove(swarmVelocity, speed, delta):
-	velocity = getRatVelocity()
+func ratMove(swarmVelocity:Vector2, speed:float, delta:float):
+	velocity = getRatVelocity(swarmVelocity, speed, delta)
+	move_and_slide()
+	currentPositionOffset += velocity
 	var globalRatVelocity:Vector2 = swarmVelocity + velocity
 	animateRat(globalRatVelocity)
 
-func getRatVelocity() -> Vector2:
-	return Vector2.ZERO
+func getRatVelocity(swarmVelocity:Vector2, speed:float, delta:float) -> Vector2:
+	# Start with a rat velocity opposite of the swarm. This will keep the rat still
+	var ratVelocity:Vector2 = swarmVelocity * -1
+	
+	# Push this frame's velocity onto our stack
+	deltaQueue.push_back(delta)
+	if positionQueue.size() == 0:
+		positionQueue.push_back(swarmVelocity)
+	else:
+		positionQueue.push_back(positionQueue[-1] + swarmVelocity)
+	
+	currentDeltaOffset += delta
+	# Get desired position
+	var desiredPosition:Vector2 = Vector2.ZERO
+	while (currentDeltaOffset > delay):
+		currentDeltaOffset -= deltaQueue.pop_front()
+		desiredPosition = positionQueue.pop_front()
+	
+	var desiredGlobalVelocity:Vector2 = (desiredPosition - currentPositionOffset).normalized() * speed
+	ratVelocity += desiredGlobalVelocity
+	
+	#print("swarmVelocity = " + str(swarmVelocity) + ", currentDeltaOffSet = " + str(currentDeltaOffset) + ", desiredGlobalVelocity = " + str(desiredGlobalVelocity) + ", ratVelocity = " + str(ratVelocity) + ", desiredPosition = " + str(desiredPosition) + ", currentPositionOffset = " + str(currentPositionOffset) +", positionQueue = " + str(positionQueue) + ", deltaQueue = " + str(deltaQueue))
+	
+	return ratVelocity
 
 # GlobalRatVelocity is the rat's velocity relative to the background (since this.velocity is relative to the pack
 func animateRat(globalRatVelocity):
