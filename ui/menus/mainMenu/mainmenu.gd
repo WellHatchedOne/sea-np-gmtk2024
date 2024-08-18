dd4ef93a4f6ceda26ee1e988b80157d1c034902b
yangn3 <yangnathank@gmail.com>
2024-08-17 21:02:33 -0700

    center main menu buttons, add placeholder fn for button hover, add hover icons

--- ui/menus/mainMenu/mainmenu.gd
+++ ui/menus/mainMenu/mainmenu.gd
@@ -8,3 +8,27 @@ func _on_options_pressed():
 	
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_exit_mouse_entered():
+	pass
+
+func _on_exit_mouse_exited():
+	pass
+
+
+func _on_play_mouse_entered():
+	
+	pass # Replace with function body.
+
+
+func _on_play_mouse_exited():
+	pass # Replace with function body.
+
+
+func _on_options_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_options_mouse_exited():
+	pass # Replace with function body.
