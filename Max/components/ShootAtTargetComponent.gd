22206b5ebbc85b92e8f5048167d55d96053bb3c5
maxhuffy <42044782+maxhuffy@users.noreply.github.com>
2024-08-18 12:40:24 -0700

    added bulletHitBox to rat and fixed daniel
    combined mecha fixed bad reference

--- Max/components/ShootAtTargetComponent.gd
+++ Max/components/ShootAtTargetComponent.gd
@@ -5,10 +5,19 @@ const BULLET = preload("res://Max/enemy/Bullet.tscn")
 @export var target: Node = null
 @export var vision_radius: float = 200
 @export var bullet_speed: float = 6
+
+@export_range(0, 3, 0.05) var fire_rate: float = 0.5
+@export_range(0, 3, 0.05) var fire_rate_variance: float = 0.5
+
 @onready var vision_circle_shape = $VisionCircleShape
+@onready var fire_rate_timer = $FireRateTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	fire_rate_timer.timeout.connect(_fire_rate_event)
+	fire_rate_timer.wait_time = fire_rate
+	
 	if (target == null):
 		target = owner.find_child("PackOfRats")
 
@@ -20,10 +29,18 @@ func shoot_at_target():
 	self.call_deferred("add_child", new_bullet)
 	new_bullet.start(self.position)
 	new_bullet.speed = bullet_speed
-	new_bullet.direction = self.position.direction_to(target.position)
+	new_bullet.direction = get_parent().position.direction_to(target.position)
+	new_bullet.start_traveling = true
+
+func _fire_rate_event():
+	shoot_at_target()
+	# Add variance to how often the shots occur
+	fire_rate_timer.wait_time = fire_rate + randf_range(-fire_rate_variance, fire_rate_variance)
 
 func _on_body_entered(body):
 	target = body
 	shoot_at_target()
+	fire_rate_timer.start()
 func _on_body_exited(body):
 	target = null
+	fire_rate_timer.stop()
--- Max/enemy/Bullet.gd
+++ Max/enemy/Bullet.gd
@@ -2,14 +2,16 @@ extends Area2D
 class_name Bullet
 
 @export var speed = 15
-@export var direction = Vector2(0.5,0.5)
+var direction = Vector2(0,0)
+var start_traveling = false
 
 func start(pos):
 	position = pos
 
 func _physics_process(delta):
-	position += direction*speed
+	if start_traveling:
+		position += direction*speed
 
 func _on_area_entered(area):
-	# Collision Layer and Mask is fucked right now
-	print(area.name)
+	if area is BulletHitboxComponent:
+		print("hitable!")
