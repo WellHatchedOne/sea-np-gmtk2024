80f66bb3fa0ba7c4a290a0f704154667e4934ae7
Nathan Yang <yangnathank@gmail.com>
2024-08-17 21:21:31 -0700

    Main menu prettify (#36)

--- ui/menus/mainMenu/mainmenu.gd
+++ ui/menus/mainMenu/mainmenu.gd
@@ -8,3 +8,25 @@ func _on_options_pressed():
 	
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_exit_mouse_entered():
+	$HoverIconExit.visible = true
+
+func _on_exit_mouse_exited():
+	$HoverIconExit.visible = false
+
+
+func _on_play_mouse_entered():
+	$HoverIconPlay.visible = true	
+
+
+func _on_play_mouse_exited():
+	$HoverIconPlay.visible = false
+
+
+func _on_options_mouse_entered():
+	$HoverIconOptions.visible = true
+
+func _on_options_mouse_exited():
+	$HoverIconOptions.visible = false
