e0b530211ae1c67532bf470a99babc2d923dc556
Shawn <shawn.hatchwell@gmail.com>
2024-08-19 11:43:02 -0700

    Add more tilesets, ability to change tileset (#68)

--- mapGen/dungeon.gd
+++ mapGen/dungeon.gd
@@ -3,9 +3,20 @@ extends Node2D
 const LEVEL = preload("res://mapGen/level.tscn")
 
 func _ready():
+	
 	var level = LEVEL.instantiate()
-	add_child(level)
-	move_child(level, 0)
+	level.tilemapAtlasId = 2
+	get_child(0).add_sibling(level) # add_child and move_child to 0, but without adding to end first
 	print(level.spawnPoint)
 	
 	$PackOfRats.position = level.spawnPoint
+	music()
+
+
+func music():
+	var music = AudioStreamPlayer.new()
+	add_child(music)
+	const default_music = preload("res://assets/Music/tune0.wav")
+
+	music.stream = default_music
+	music.play()
