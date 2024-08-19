2cede46fa161441e51b58d47fc2eb845d5ea9ab5
drentawc <wdrenta123@gmail.com>
2024-08-18 20:30:35 -0700

    Fixing pause menu pop up functionality

--- ui/menus/pauseMenu/pausemenu.gd
+++ ui/menus/pauseMenu/pausemenu.gd
@@ -1,11 +1,39 @@
 extends Control
 
+@onready var pauseMenu = $Pausemenu
+@onready var window = $Window	
+	
+func _on_resume_pressed():
+	print("resume")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func _on_ratabase_pressed():
+	print("ratabase")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_quit_pressed():
+	print("quit")
+	get_tree().quit()
+	
+	
+func _on_quit_mouse_entered():
+	$HoverIconQuit.visible = true
+
+func _on_quit_mouse_exited():
+	$HoverIconQuit.visible = false
+
+
+func _on_resume_mouse_entered():
+	$HoverIconResume.visible = true	
+
+
+func _on_resume_mouse_exited():
+	$HoverIconResume.visible = false
+
+
+func _on_ratabase_mouse_entered():
+	$HoverIconOptions.visible = true
+
+func _on_ratabase_mouse_exited():
+	$HoverIconOptions.visible = false
+
