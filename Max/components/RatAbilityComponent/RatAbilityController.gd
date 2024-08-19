3b3fa2115f82b9462db6e97a14b150df1d8329e1
maxhuffy <42044782+maxhuffy@users.noreply.github.com>
2024-08-18 21:29:31 -0700

    Max many bullets changes (#59)

---
+++ Max/components/RatAbilityComponent/RatAbilityController.gd
@@ -0,0 +1,22 @@
+extends Node2D
+class_name RatAbilityController
+
+const BULLET = preload("res://Max/enemy/Bullet.tscn")
+@export_range(0,2,0.01) var fire_rate: float = 0.5
+@export_range(0,10,0.1) var bullet_speed: float = 5
+@export_range(0,20,0.25) var bullet_lifetime_seconds: float = 3
+@export var debug: bool = true
+@onready var timer = $Timer
+
+var bullet_direction_array: Array[BulletDestination] = []
+@onready var bullet_origin = $BulletOrigin
+
+func _ready():	
+	for child in self.get_children():
+		if child is BulletDestination:
+			child.start_firing()
+			
+			
+			child.color_rect.visible = debug
+			bullet_origin.color_rect.visible = debug
+	
