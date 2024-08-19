f67d8ae334d3b8725115234bb2bfa44fb5eb5105
hackda-code <hackdah101@gmail.com>
2024-08-19 11:57:54 -0700

    component for enemy to move toward rat (#65)
    Co-authored-by: Shawn <shawn.hatchwell@gmail.com>

---
+++ Daniel/enemyprotype.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@onready var move_at_target_component = $MoveAtTargetComponent
+
+@export var should_move: bool = true
+@export var vel: float = 100
+
+var dir = Vector2(1,1)
+
+func _ready():
+	for child in get_children():
+		if child is MoveAtTargetComponent:
+			move_at_target_component = child
+			break
+
+func _physics_process(delta):
+	if should_move == true:
+		
+		dir = move_at_target_component.direction
+		velocity = dir*vel
+		move_and_slide()
