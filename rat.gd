75175572110bf4b18702abee1ffab4b0b29de44b
ianbrobin <ianbrobingit@gmail.com>
2024-08-18 22:42:38 -0700

    Fix bug where rats turn to the left on stop (#63)

--- rat.gd
+++ rat.gd
@@ -35,8 +35,9 @@ func set_delay(newDelay:float):
 # Moves the rat for this frame relative to the swarm
 func ratMove(swarmVelocity:Vector2, speed:float, delta:float):
 	var desiredGlobalPosition:Vector2 = getRatDesiredGlobalPosition(swarmVelocity, speed, delta)
-	moveAndSlideRat(desiredGlobalPosition, swarmVelocity, speed, delta)
-	animateRat(swarmVelocity)
+	var shouldAnimate = moveAndSlideRat(desiredGlobalPosition, swarmVelocity, speed, delta)
+	if shouldAnimate:
+		animateRat(swarmVelocity)
 
 func getRatDesiredGlobalPosition(swarmVelocity:Vector2, speed:float, delta:float) -> Vector2:
 	# Push this frame's velocity onto our queue of time and position offsets
@@ -54,7 +55,8 @@ func getRatDesiredGlobalPosition(swarmVelocity:Vector2, speed:float, delta:float
 
 	return desiredPosition
 
-func moveAndSlideRat(desiredGlobalPosition: Vector2, swarmVelocity:Vector2, speed:float, delta:float):
+# Returns true if motion should be animated, false if not
+func moveAndSlideRat(desiredGlobalPosition: Vector2, swarmVelocity:Vector2, speed:float, delta:float) -> bool:
 	if (position.distance_to(Vector2.ZERO) < DISTANCE_FROM_PACK_TO_START_MOVING):
 		currentRatState = RatState.FOLLOWING
 
@@ -64,7 +66,7 @@ func moveAndSlideRat(desiredGlobalPosition: Vector2, swarmVelocity:Vector2, spee
 		# Make the rats stay stil if they are too far away (technically, they have to move to oppose the pack)
 		velocity = -1 * swarmVelocity
 		move_and_slide()
-		return
+		return false
 
 	var desiredGlobalVelocity:Vector2 = (desiredGlobalPosition).normalized() * speed
 	if(swarmVelocity == Vector2.ZERO && desiredGlobalVelocity == Vector2.ZERO):
@@ -72,12 +74,15 @@ func moveAndSlideRat(desiredGlobalPosition: Vector2, swarmVelocity:Vector2, spee
 		velocity = vectorToStartingPosition.normalized() * speed
 		if vectorToStartingPosition.length() > 5:
 			move_and_slide()
+			return true
 		else:
 			# Move back to starting position when rat and frame isn't moving
 			position = position.move_toward(startingPosition, speed * delta)
+			return false
 	else:
 		velocity = desiredGlobalVelocity - swarmVelocity
 		move_and_slide()
+		return true
 
 # GlobalRatVelocity is the rat's velocity relative to the background (since this.velocity is relative to the pack)
 func animateRat(swarmVelocity:Vector2):
