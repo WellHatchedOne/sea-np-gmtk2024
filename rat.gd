b24140815e61a2889ff5ddcf24157df7c3a7da0c
ianbrobin <ianbrobingit@gmail.com>
2024-08-20 02:08:07 -0700

    Add rat cluster cooldown (#100)

--- rat.gd
+++ rat.gd
@@ -29,6 +29,9 @@ func _ready():
 func getStartingPosition() -> Vector2:
 	return startingPosition
 
+func getRatState() -> RatState:
+	return currentRatState
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
@@ -156,6 +159,7 @@ func animateRat(swarmVelocity:Vector2, shouldAnimateTurn:bool):
 		animated_sprite_2d.animation = "sitting"
 		animated_sprite_2d.rotation = (deg_to_rad(0))
 		self.rotation = (deg_to_rad(0))
+		animated_sprite_2d.offset = defaultRatOffset
 
 	if !shouldAnimateTurn:
 		return
