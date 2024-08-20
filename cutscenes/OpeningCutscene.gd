f7c78a1855e15a4c771fe6f2b07a3766fcbea923
ben <bensmmail@gmail.com>
2024-08-19 19:42:08 -0700

    Resize opening cutscene and add final assets

--- cutscenes/OpeningCutscene.gd
+++ cutscenes/OpeningCutscene.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 
 func _ready():
+	var gain_power_animated_sprite = $Scenes/GainPower
+	gain_power_animated_sprite.play("GainPower")
+	var rat_moving_animated_sprite = $Scenes/RatMoving
+	rat_moving_animated_sprite.play("RatMoving")
 	$AnimationPlayer.play()
-
-
-
 
 func _on_button_pressed():
 	print("Hello world!")
