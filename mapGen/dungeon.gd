68e85a32188e666b0a5eaacd1e9914126e8a3648
Shawn <shawn.hatchwell@gmail.com>
2024-08-19 22:30:59 -0400

    Fix lab corners, level loading (#77)
    Co-authored-by: maxhuffy <42044782+maxhuffy@users.noreply.github.com>

--- mapGen/dungeon.gd
+++ mapGen/dungeon.gd
@@ -44,10 +44,8 @@ func load_new_level(levelNum : int):
 	var level = LEVEL.instantiate()
 	print("level:" + level.name)
 	level.tilemapAtlasId = levelNum
-	##get_child(0).add_sibling(level) # add_child and move_child to 0, but without adding to end first
+	get_child(0).add_sibling(level) # add_child and move_child to 0, but without adding to end first
 	levelPointer = level
-	add_child(level)
-	move_child(level, 0)
 	print(level.spawnPoint)
 	
 	$PackOfRats.position = level.spawnPoint
