6bca4f88fbf686d1f15bf53caff784cc01cf7f07
hackda-code <hackdah101@gmail.com>
2024-08-20 09:01:16 -0700

    Win and number rats goal (#129)

---
+++ ui/huds/hud/Label3.gd
@@ -0,0 +1,51 @@
+extends Label
+
+@onready var animation_player = $AnimationPlayer
+var levelnum = 1
+var score = 5
+var red
+var blue 
+var green
+var firstplay = true
+func _ready():
+	#$pack_of_rats.connect("ratsignal",self, "on_ratsignal")
+	#print(owner.Player.ratnumber)
+	score = owner.Player.ratnumber
+	text =""
+	#levelnum = get_tree().root.get_child(0).get_node("Level").level.tilemapAtlasId
+	#print(get_tree().root.get_child(0).get_children())
+	
+func _process(delta):
+	score = owner.Player.ratnumber
+	#print(score)
+	
+	if getPlayerLevel() >= 3 and firstplay == true:
+		_activate()
+		firstplay = false
+		$Timer.start()
+		
+	else:
+		_deactivate()
+
+		
+func _activate():
+	text ="YOU WIN"
+	animation_player.play()
+	
+func _deactivate():
+	pass
+	#text = ""
+	#animation_player.stop()
+
+func  getPlayerTarget() -> float:
+	return getLevelNode().getPlayerTarget()
+
+func getPlayerLevel() -> float:
+	return getLevelNode().playerLevel
+
+func getLevelNode() -> Level:
+	return get_parent().get_parent().get_parent().get_node("Level")
+
+
+func _on_timer_timeout():
+	_deactivate()
