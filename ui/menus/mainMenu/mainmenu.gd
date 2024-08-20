708a6145272c39ac490fd2b842267ecba042b284
Ian Brobin <ianbrobin@yahoo.com>
2024-08-20 04:17:12 -0700

    Undo any changes to main menu

--- ui/menus/mainMenu/mainmenu.gd
+++ ui/menus/mainMenu/mainmenu.gd
@@ -5,6 +5,7 @@ extends Control
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://cutscenes/OpeningCutscene.tscn")
 
+
 func _on_options_1_pressed():
 	get_tree().change_scene_to_file("res://ui/menus/optionsMenu/optionsMenu.tscn")
 	print("pressed")
